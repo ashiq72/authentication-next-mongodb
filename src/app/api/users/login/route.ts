37b390cf5bd83924527995b79153b3176a7e5c5a
Md ashiquzzaman <ahmedashik18k@gmail.com>
2024-01-01 12:55:12 +0600

    middleware added

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -37,7 +37,7 @@ export async function POST(request: NextRequest) {
 
     //create token
     const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
-      expiresIn: "2m",
+      expiresIn: "30ms",
     });
 
     const response = NextResponse.json({
