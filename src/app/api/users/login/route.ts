db7f9cf266e62f009ef5c5d3cbdc40ef064b3033
Md ashiquzzaman <ahmedashik18k@gmail.com>
2024-01-03 17:44:42 +0600

    token expireIn increase

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -37,7 +37,7 @@ export async function POST(request: NextRequest) {
 
     //create token
     const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
-      expiresIn: "30ms",
+      expiresIn: "h1",
     });
 
     const response = NextResponse.json({
